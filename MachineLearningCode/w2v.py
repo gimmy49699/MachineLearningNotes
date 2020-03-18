27033ae31a5e5082b807c079f22615c0a2cd307b
Jinbin <420607689@qq.com>
2020-03-18 12:25:10 +0800

    Add files via upload

---
+++ MachineLearningCode/w2v.py
@@ -0,0 +1,154 @@
+'''
+    word2vec:
+    dataset: "词性标注@人民日报199801.txt"
+'''
+import os
+import pickle, time
+import numpy as np
+
+
+def softmax(x):
+    ex = np.exp(x)
+    return ex/np.sum(ex)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class word2vec(object):
+    """docstring for word2vec"""
+    def __init__(self):
+        super(word2vec, self).__init__()
+
+    def _process_text(self, filepath, minfreq):
+        # pre-processing
+        with open(filepath, encoding='utf8') as corpus:
+            self.countwords = {}
+            self.rawdata, self.data = [], []
+            for sen in corpus.readlines():
+                tmp = []
+                for wordpos in sen.split(' ')[1:-1]:
+                    if wordpos == "":
+                        continue
+                    word, pos = wordpos.split('/')
+                    if '[' in word: word = word.replace('[', '')
+                    if word not in self.countwords.keys():
+                        self.countwords[word] = 1
+                    else:
+                        self.countwords[word] += 1
+                    tmp.append(word)
+                if tmp == []: continue
+                self.rawdata.append(tmp)
+        # build id2vocab and vocab2id
+        self.vocab2id, self.id2vocab = {'unk':0}, {0:'unk'}
+        sortvocab = sorted([[v, k] for k, v in self.countwords.items()])
+        sortvocab = [[v, k] for v, k in sortvocab if v > minfreq]
+        idx = 1
+        for v, k in sortvocab:
+            self.vocab2id[k] = idx
+            self.id2vocab[idx] = k
+            idx += 1
+        self.vocabsize = len(self.vocab2id)
+        for datas in self.rawdata:
+            self.data.append([self.vocab2id[w] if w in self.vocab2id.keys() else 0 for w in datas])
+
+    def _train_tgt(self, windowsize):
+        self.traindata = []
+        for intdata in self.data:
+            for idx in range(len(intdata)):
+                srtidx = idx - windowsize if (idx - windowsize) > 0 else 0
+                endidx = idx + windowsize if (idx + windowsize) < len(intdata) - 2 else len(intdata) - 2
+                tgtwords = intdata[srtidx:idx] + intdata[idx+1:endidx+1]
+                for tgt in tgtwords:
+                    self.traindata.append([intdata[idx], tgt])
+        self.traindata = np.array(self.traindata)
+
+    def _init_vec(self, vocabsize, vecsize):
+        self.lookuptable = np.random.normal(loc=0.0, scale=0.25, size=(vocabsize, vecsize))
+        self.weight = np.random.normal(loc=0.0, scale=0.25, size=(vocabsize, vecsize))
+
+    def _forward_backward(self, x, y, lr, K):
+        # forward
+        inputx = self.lookuptable[x]
+        targety = self.weight[y]
+        outputprobs = sigmoid(np.dot(inputx, targety))
+
+        # backward
+        samples = self._negsample(y, K)
+        delta_w = (outputprobs - 1.0) * self.weight[y]
+        self.weight[y] -= lr*(outputprobs - 1.0)*inputx
+        loss = -np.log(outputprobs + 1e-5)
+        for idx in samples:
+            tmpoutput = sigmoid(np.dot(inputx, self.weight[idx]))
+            loss -= np.log(tmpoutput + 1e-5)
+            delta_w += tmpoutput * self.weight[idx]
+            self.weight[idx] -= lr*delta_w*inputx
+        self.lookuptable[x] -= lr*delta_w
+        return loss
+
+    def _negsample(self, y, K):
+        res = [None]*K
+        for idx in range(K):
+            tmpidx = np.random.randint(0, self.vocabsize)
+            while tmpidx == y:
+                tmpidx = np.random.randint(0, self.vocabsize)
+            res[idx] = tmpidx
+        return res
+
+    def _train_skipgram(self, traindata, epochs, lr, K, vecsize):
+        self._init_vec(self.vocabsize, vecsize)
+        print('Start training...!')
+        # training
+        for epoch in range(1, epochs+1):
+            if epoch > 2:
+                lr = lr/2
+            loss, itertimes = 0, 1
+            strtime = time.time()
+            for x, y in traindata:
+                loss += self._forward_backward(x, y, lr, K)
+                if itertimes % 1000 == 0:
+                    stptime = time.time()
+                    showmsg = 'Epoch {:>2d}/{:>2d} - itertimes: {:>7d} - loss:{:>.4f} - {:>.4f} sec/1000'.format(
+                        epoch, epochs, itertimes, loss/1000, stptime-strtime)
+                    print(showmsg)
+                    loss = 0
+                    strtime = time.time()
+                # save
+                if itertimes % 500000 == 0:
+                    pickle.dump(self.lookuptable, open(r'.\data\wordvectors.pkl', 'wb'))
+                itertimes += 1
+
+    def _cheak(self, shownum, k):
+        if not os.path.exists(r'.\data\wordvectors.pkl'):
+            raise ValueError('No word vectors found!')
+        self.lookuptable = pickle.load(open(r'.\data\wordvectors.pkl', 'rb'))
+        random_chose = [np.random.randint(0, self.vocabsize) for _ in range(shownum)]
+        vecnorm = [np.linalg.norm(vec, ord=2) for vec in self.lookuptable]
+        for choice in random_chose:
+            tmp = []
+            for idx in range(self.vocabsize):
+                # cosine distance
+                # tmp.append(np.dot(self.lookuptable[idx], self.lookuptable[choice]) / (vecnorm[idx]*vecnorm[choice]))
+                # 2-norm distance
+                tmp.append(np.linalg.norm((self.lookuptable[idx] - self.lookuptable[choice]), ord=2))
+            topk = [int(np.argwhere(np.argsort(tmp)==i)) for i in range(k)]
+            topkneighbors = [self.id2vocab[idx] for idx in topk]
+            showmsg = 'Top '+str(k) + ' words nearest to [{:^6}] : '.format(self.id2vocab[choice])
+            res = ', '.join(topkneighbors)
+            print(showmsg+res)
+
+
+if __name__ == "__main__":
+    # loading dataset
+    filepath = r'.\data\词性标注@人民日报199801.txt'
+    # building word2vec model
+    w2v = word2vec()
+    # pre-processing data
+    w2v._process_text(filepath=filepath, minfreq=10)
+    # generating training data
+    w2v._train_tgt(windowsize=2)
+    # training word vectors
+    # w2v._train_skipgram(traindata=w2v.traindata, epochs=5, lr=0.001, K=20, vecsize=100)
+    # show res
+    w2v._cheak(10, 5)
